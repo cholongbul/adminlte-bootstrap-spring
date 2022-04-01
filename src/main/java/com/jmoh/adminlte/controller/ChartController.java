1599782c544a89095b622b707a683f27b63e4f6e
ejatk <meyoub612@naver.com>
2022-04-01 13:49:18 +0900

    Initial commit

---
+++ src/main/java/com/jmoh/adminlte/controller/ChartController.java
@@ -0,0 +1,31 @@
+package com.jmoh.adminlte.controller;
+
+import com.jmoh.adminlte.apigetter.ApiExplorer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class ChartController {
+
+    @GetMapping("/")
+    public String chart(Model model){
+        return "index";
+    }
+
+
+    @GetMapping("getData.do")
+    @ResponseBody
+    public String getElectiondata(HttpServletResponse response, HttpServletRequest request) throws Exception {
+        String page = request.getParameter("page");
+        ApiExplorer apiExplorer = new ApiExplorer();
+        apiExplorer.setXMLPATH();
+        response.addIntHeader("xmlcnt", apiExplorer.getFilecnt());
+
+        return apiExplorer.getDataAPI(Integer.parseInt(page));
+    }
+}
